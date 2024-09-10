060cff2c661a8508291f1701f8c20bc79bc88383
CapybaraYT1 <Capybarayt2.official@gmail.com>
2024-09-10 18:06:35 +0300

    Add files via upload

---
+++ mdl.js
@@ -0,0 +1,10 @@
+function showmodal(){
+    document.getElementById("modal").style.display="block"
+    document.getElementById("cnopka").style.display="none"
+
+}
+
+function closemodal(){
+    document.getElementById("modal").style.display="none"
+        document.getElementById("cnopka").style.display="inline"
+}
